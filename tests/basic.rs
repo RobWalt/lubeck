06254f01a984c9bec7467886b036aa4710e96547
RobWalt <robwalter96@gmail.com>
2022-05-09 23:42:08 +0200

    feat(basics): Implement fp basics on my own
    Implement traits:
        - Semigroup
        - Monoid
        - Functor
        - Applicative
        - Monad
    guided by Haskell typeclasses. Additionally implement HKT trait as a
    hack to make the other traits possible and implement traits:
        - Pure
        - Return
    for better ergonomics (they can be used without specifying Applicative
    and Monad respectively).

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,6 +1,13 @@
-use lubeck::add_one;
+use lubeck::{Monad, Return};
 
 #[test]
-fn one_plus_one() {
-    assert_eq!(2, add_one(1));
+fn monad_option() {
+    fn add_one(x: i32) -> Option<i32> {
+        Some(x + 1)
+    }
+
+    let x = Option::r#return(1);
+    let y = x.bind(add_one);
+
+    assert_eq!(Some(2), y);
 }
